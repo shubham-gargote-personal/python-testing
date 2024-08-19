c866b97a8a3ec297a97d3b79705f839700835cc7
shubham-gargote-personal <shubhqastudy@gmail.com>
2024-08-19 16:55:57 +0530

    "first commit"

---
+++ boolean_logic.py
@@ -0,0 +1,16 @@
+# and (&) operator
+a = 3
+b = 4
+
+print(a == 3 and b == 5) # both conditions need to be True
+
+
+# or operator
+
+print (a == 3 or b == 5) # one of the condition needs to be true
+
+
+#combination of and OR or
+
+print(a == 3 and b == 5 or b == 4)
+
